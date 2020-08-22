3db4550d77ceec24fa6367f5faede0f55e759d4b
Bryan Beshore <bryan.beshore@gmail.com>
2020-08-22 10:40:35 -0400

    Added subdomains to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_205945) do
+ActiveRecord::Schema.define(version: 2020_08_22_142656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_08_21_205945) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "owner_id"
+    t.string "subdomain"
+    t.index ["subdomain"], name: "index_accounts_on_subdomain"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
